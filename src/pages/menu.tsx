0f5b84cf50897796fe53d30945a01f93ee819ab6
aninprmrt <anindita.paramarta@gmail.com>
2023-12-08 01:31:03 +0700

    feat: added menu

---
+++ src/pages/menu.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const Menu: React.FC = () => {
+    return (
+        
+            <div className="w-[96px] h-[24px]">
+                <div className="fixed top-0 left-0 [font-family:'Lato-Bold',Helvetica] font-bold text-primary-color-1sub-7 text-[20px] tracking-[0] leading-[normal] whitespace-nowrap">
+                    Categories
+                </div>
+            </div>
+
+    )
+}
+export default Menu
+
