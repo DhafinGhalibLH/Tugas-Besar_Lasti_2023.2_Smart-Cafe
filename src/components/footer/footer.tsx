7e6ae88337fe90f99ea10054d6a819b7d7b26753
aninprmrt <anindita.paramarta@gmail.com>
2023-12-08 21:03:17 +0700

    feat(menu): update footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <div>
-            <Image src="images/footer.svg" alt="footer" width={375} height={195}/>
+            <Image src="images/footer.svg" alt="footer" width={390} height={195}/>
         </div>
     )
 }
