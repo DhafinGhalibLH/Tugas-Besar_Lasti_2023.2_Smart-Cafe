e396acd93ef589316bc66cd7375ec60141945e1b
anjanibrhm <anjaniibrahimm@gmail.com>
2023-12-08 19:34:47 +0700

    update ga guna real

---
+++ src/pages/reservation.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const Reservation: React.FC = () => {
+
+    return (
+
+        <div className=' h-28 flex items-center justify-center text-3xl'>
+            Reservation
+        </div>
+        
+    )
+
+
+
+};
+
+export default Reservation;
